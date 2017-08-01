d48bac738f44432c79e5275d6475696c3e225e4c
Chris Bay <chris@chrisbay.net>
2017-08-01 17:55:44 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/flickrsearch/FlickrSearchApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.flickrsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickrSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickrSearchApplication.class, args);
+	}
+}
