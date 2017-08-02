71f7772bd8b285775926071e330d9eae96447ba3
Chris Bay <chris@chrisbay.net>
2017-08-02 10:54:46 -0500

    Rename FlickrConnector -> FlickrPublicConnector

--- src/main/java/org/launchcode/flickrsearch/controllers/SearchController.java
+++ src/main/java/org/launchcode/flickrsearch/controllers/SearchController.java
@@ -1,9 +1,8 @@
 package org.launchcode.flickrsearch.controllers;
 
-import org.launchcode.flickrsearch.models.FlickrConnector;
+import org.launchcode.flickrsearch.api.FlickrPublicConnector;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,7 +14,7 @@ public class SearchController {
 
     @RequestMapping(value = "tags/{tags}")
     public String getFlickrData(@PathVariable String[] tags){
-        return FlickrConnector.fetchImageJson(tags);
+        return FlickrPublicConnector.fetchImageJson(tags);
     }
 
 }
